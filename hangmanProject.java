cc8523f53449c0479f994fca81fe23246a140832
Alex-935 <alex.potter3@hotmail.co.uk>
2025-08-09 08:01:48 +0100

    Hangman Java Project

---
+++ hangmanProject.java
@@ -0,0 +1,22 @@
+public class hangmanProject {
+    
+    public static void main(String[] args) {
+
+        /* 
+        //Pick a random word from list of strings
+        //display hidden version of word
+        //ask user for input
+        //check input against word
+            - fill in any correct letters
+            - add to gallows if no matches
+        //Draw in gallows
+
+        // if incorrect guesses = num
+            - agame over
+            - reveal word
+        
+            // play again?
+        */
+
+    }
+}
